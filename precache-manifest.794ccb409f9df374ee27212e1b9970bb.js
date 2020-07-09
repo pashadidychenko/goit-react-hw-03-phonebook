6398f5ec5d0a49694bc46770597879bf90ab101a
pashadidychenko <pasha.didychenko@gmail.com>
2020-07-09 19:49:15 +0300

    Updates

---
+++ precache-manifest.794ccb409f9df374ee27212e1b9970bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a8541bb0bc9651de0356833fd62c04",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "78ea749244323e2e0a18",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.c9765f85.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.c9765f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8bb7573ae7fbed468fb",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.72d404d5.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pashadidychenko/goit-react-hw-03-phonebook/precache-manifest.d078f867258a3b65464466f896c4872c.js"
+  "/goit-react-hw-03-phonebook/precache-manifest.794ccb409f9df374ee27212e1b9970bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pashadidychenko/goit-react-hw-03-phonebook/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-phonebook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
